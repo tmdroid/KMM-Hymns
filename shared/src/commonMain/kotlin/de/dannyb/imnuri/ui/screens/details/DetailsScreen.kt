67b4467c1fbce2c4090213537d7f544909040539
daniel <daniel@simvelop.de>
2023-05-21 05:15:05 +0200

    fix scroll and zoom behaviour

--- shared/src/commonMain/kotlin/de/dannyb/imnuri/ui/screens/details/DetailsScreen.kt
+++ shared/src/commonMain/kotlin/de/dannyb/imnuri/ui/screens/details/DetailsScreen.kt
@@ -1,6 +1,8 @@
 package de.dannyb.imnuri.ui.screens.details
 
 import androidx.compose.foundation.gestures.detectTransformGestures
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,12 +12,12 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Favorite
 import androidx.compose.material.icons.rounded.FavoriteBorder
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -23,6 +25,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.arkivanov.decompose.ComponentContext
@@ -59,51 +62,80 @@ fun DetailsScreen(
     onFavoriteAction: (Hymn) -> Boolean,
 ) = Column(Modifier.fillMaxSize()) {
 
-    var fontSize by remember { mutableStateOf(20f) }
     var isFavorite by remember { mutableStateOf(false) }
+
+    DetailsToolbar(component, isFavorite, onFavoriteAction)
+    ShowZoomableHymn(component)
+}
+
+@Composable
+fun ShowZoomableHymn(component: HymnDetailsComponent) {
+    val fontSize = remember { mutableStateOf(30f) }
+
+    val zoomableModifier = Modifier.pointerInput(Unit) {
+        detectTransformGestures { _, _, zoom, _ ->
+            fontSize.value *= zoom
+        }
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .then(zoomableModifier),
+        contentAlignment = Alignment.TopCenter
+    ) {
+        ScrollableColumn(component, fontSize)
+    }
+}
+
+@Composable
+fun ScrollableColumn(
+    component: HymnDetailsComponent,
+    fontSize: MutableState<Float>,
+) {
+    val scrollState = rememberScrollState()
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .verticalScroll(scrollState),
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Spacer(modifier = Modifier.fillMaxWidth().height(40.dp))
+
+        Text(
+            modifier = Modifier.align(alignment = Alignment.CenterHorizontally),
+            text = component.hymn.stanzas.joinToString("\n\n"),
+            fontSize = fontSize.value.sp,
+            textAlign = TextAlign.Center,
+        )
+
+        Spacer(modifier = Modifier.fillMaxWidth().height(40.dp))
+    }
+}
+
+@Composable
+private fun DetailsToolbar(
+    component: HymnDetailsComponent,
+    isFavorite: Boolean,
+    onFavoriteAction: (Hymn) -> Boolean
+) {
+    var isFavorite1 = isFavorite
 
     Toolbar(
         title = component.hymn.title,
         onNavigateBack = { component.onBackClicked() },
         rightIcons = {
             IconButton(onClick = {
-                isFavorite = !isFavorite
+                isFavorite1 = !isFavorite1
                 onFavoriteAction.invoke(component.hymn)
             }) {
                 Icon(
-                    imageVector = if (isFavorite) Icons.Rounded.Favorite else Icons.Rounded.FavoriteBorder,
+                    imageVector = if (isFavorite1) Icons.Rounded.Favorite else Icons.Rounded.FavoriteBorder,
                     contentDescription = "Favorites",
                 )
             }
         }
     )
-
-    val zoomableModifier = Modifier.pointerInput(Unit) {
-        detectTransformGestures { _, _, zoom, _ ->
-            fontSize *= zoom
-        }
-    }
-
-    Surface(
-        modifier = zoomableModifier.fillMaxSize()
-    ) {
-
-        Column(
-            modifier = Modifier.fillMaxWidth()
-                .verticalScroll(rememberScrollState())
-        ) {
-
-            Spacer(modifier = Modifier.fillMaxWidth().height(40.dp))
-
-            Text(
-                text = component.hymn.stanzas.joinToString("\n\n"),
-                fontSize = fontSize.sp,
-                modifier = zoomableModifier.align(alignment = Alignment.CenterHorizontally)
-            )
-
-            Spacer(modifier = Modifier.fillMaxWidth().height(40.dp))
-
-        }
-
-    }
 }
